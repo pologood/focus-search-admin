5b30c2e1b24f9c2b26fd046e41ca470b03f0d9ac
qingyuan <qingyuanxue@sohu-inc.com>
2015-08-10 16:18:33 +0800

    优化轮询应答。

--- src/main/java/cn/focus/search/admin/controller/RemoteDicController.java
+++ src/main/java/cn/focus/search/admin/controller/RemoteDicController.java
@@ -1,6 +1,7 @@
 package cn.focus.search.admin.controller;
 
 
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -21,7 +22,7 @@ public class RemoteDicController {
 	
 	@RequestMapping("remote_final_house.dic")
 	@ResponseBody
-	public String getRemoteFinalHouseDic(HttpServletResponse response){
+	public String getRemoteFinalHouseDic(HttpServletResponse response,HttpServletRequest request){
 	    // first time through - set last modified time to now 
 		Logger logger = LoggerFactory.getLogger(RemoteDicController.class);
  		response.setDateHeader("Last-Modified", LastTime.final_house_lTime);
@@ -30,13 +31,15 @@ public class RemoteDicController {
  		response.setCharacterEncoding("UTF-8");
 
   		logger.info("Writing word to  remote_final_house.dic");
-		return participleManagerService.getRemoteFinalHouseWord();
+  		long temp=request.getDateHeader("If-Modified-Since");
+  		if (temp!=-1&&temp==LastTime.final_house_lTime) return "";
+  		else return participleManagerService.getRemoteFinalHouseWord();
 		
 	}
 	
 	@RequestMapping("remote_stopword.dic")
 	@ResponseBody
-	public String getRemoteStopwordDic(HttpServletResponse response){
+	public String getRemoteStopwordDic(HttpServletResponse response,HttpServletRequest request){
 		Logger logger = LoggerFactory.getLogger(RemoteDicController.class);
  		response.setDateHeader("Last-Modified", LastTime.stopword_lTime);
  		response.setHeader("ETags", "etagSting");
@@ -44,13 +47,15 @@ public class RemoteDicController {
  		response.setCharacterEncoding("UTF-8");
 
   		logger.info("Writing word to  remote_stopword.dic");
-		return participleManagerService.getRemoteStopword();
+  		long temp=request.getDateHeader("If-Modified-Since");
+  		if (temp!=-1&&temp==LastTime.stopword_lTime) return "";
+  		else return participleManagerService.getRemoteStopword();
 		
 	}
 	
 	@RequestMapping("remote_hotword.dic")
 	@ResponseBody
-	public String getRemoteHotwordDic(HttpServletResponse response){
+	public String getRemoteHotwordDic(HttpServletResponse response,HttpServletRequest request){
 		Logger logger = LoggerFactory.getLogger(RemoteDicController.class);
  		response.setDateHeader("Last-Modified", LastTime.hotword_lTime);
  		response.setHeader("ETags", "etagSting");
@@ -58,7 +63,9 @@ public class RemoteDicController {
  		response.setCharacterEncoding("UTF-8");
 
   		logger.info("Writing word to  remote_hotword.dic");
-		return participleManagerService.getRemoteHotword();
+  		long temp=request.getDateHeader("If-Modified-Since");
+  		if (temp!=-1&&temp==LastTime.hotword_lTime) return "";
+  		else return participleManagerService.getRemoteHotword();
 		
 	}
 	
@@ -66,7 +73,7 @@ public class RemoteDicController {
 	@ResponseBody
 	public String reloadRemoteDic(HttpServletResponse response){
 		Logger logger = LoggerFactory.getLogger(RemoteDicController.class);
-  		logger.info("Writing word to  remote_hotword.dic");
+		logger.info("ready to  reload total word.dic");
   		if(participleManagerService.reloadRemoteDic()) return JSONUtils.ok();
   		else return JSONUtils.badResult("failed");
 
